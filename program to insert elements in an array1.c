d5f4516525cdfbbd98cdf1cab1a2c059e5e6972c
Rohinish2005 <146616085+Rohinish2005@users.noreply.github.com>
2023-11-20 21:40:13 +0530

    Add files via upload

---
+++ program to insert elements in an array1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main(){
+	int arr[30],ele,num,i,loc;
+	printf("Enter the size of an array");
+	scanf("%d",&num);
+	for(i=0;i<num;i++){
+		printf("Enter the elements arr[%d]",i);
+		scanf("%d",&arr[i]);
+	}
+	printf("Enter the element to insert");
+	scanf("%d",&ele);
+	printf("Enter the location");
+	scanf("%d",&loc);
+	for(i=num;i>=loc;i--)
+	{
+		arr[i]=arr[i-1];}
+		num++;
+		arr[loc-1]=ele;
+		
+		for(i=0;i<num;i++){
+			printf("%d \t",arr[i]);
+	}
+		printf("\n");
+		
+}
