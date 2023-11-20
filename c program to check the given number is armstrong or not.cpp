1f8f45fa541be9274639205831f8d19e8a802e1d
Rohinish2005 <146616085+Rohinish2005@users.noreply.github.com>
2023-11-20 21:41:22 +0530

    Add files via upload

---
+++ c program to check the given number is armstrong or not.cpp
@@ -0,0 +1,24 @@
+//write a program to check  the given number is armstrong or not
+#include <stdio.h>
+int main() {
+    int num, originalNum, remainder, result = 0;
+    printf("Enter the number : ");
+    scanf("%d", &num);
+    originalNum = num;
+
+    while (originalNum != 0) {
+	    remainder = originalNum % 10;
+        
+       result += remainder * remainder * remainder;
+        
+       originalNum /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+
+    return 0;
+}
+
